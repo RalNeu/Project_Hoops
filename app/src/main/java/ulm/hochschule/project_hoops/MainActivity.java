bed5fba8b84560472b229867ddac0f5f995daad9
RalNeu <neumann.ralph@gmx.de>
2016-04-30 21:51:02 +0200

    Fixing Gallery-Button

--- app/src/main/java/ulm/hochschule/project_hoops/MainActivity.java
+++ app/src/main/java/ulm/hochschule/project_hoops/MainActivity.java
@@ -105,8 +105,7 @@ public class MainActivity extends AppCompatActivity
         if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
-            Intent regintent = new Intent(MainActivity.this,RegisterActivity.class);
-            MainActivity.this.startActivity(regintent);
+
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
