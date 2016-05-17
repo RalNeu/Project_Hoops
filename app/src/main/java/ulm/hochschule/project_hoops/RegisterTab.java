4852590c51365e5d75cba43d9459adee8ca0596e
PWeigerstorfer <patrickweigerstorfer@gmx.de>
2016-05-17 12:44:54 +0200

    RegisterTab and SqlManager changed.

--- app/src/main/java/ulm/hochschule/project_hoops/RegisterTab.java
+++ app/src/main/java/ulm/hochschule/project_hoops/RegisterTab.java
@@ -60,99 +60,10 @@ public class RegisterTab extends Fragment {
             @Override
             public void onClick(View view) {
 
-                try {
-                    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-                    StrictMode.setThreadPolicy(policy);
-
-                    CookieHandler.setDefault( new CookieManager( null, CookiePolicy.ACCEPT_ALL ));
-                    Connection con;
-                    Statement st;
-                    ResultSet rs;
-
-                    Class.forName("com.mysql.jdbc.Driver");
-
-                    con = DriverManager.getConnection("jdbc:mysql://141.59.26.107:3306/hoops", "SuperUser", "root");
-                    st = con.createStatement();
-
-                    Calendar calendar = Calendar.getInstance();
-                    java.sql.Date startDate = new java.sql.Date(calendar.getTime().getTime());
-
-                    // the mysql insert statement
-                    String query = " insert into users (Username, Password, EmailAdress, FirstName, LastName, Coins, ChatBan, CreateDate, LastLogin)"
-                            + " values (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-
-                    // create the mysql insert preparedstatement
-                    PreparedStatement preparedStmt = con.prepareStatement(query);
-
-                    preparedStmt.setString (1, "xXx_hansJo_xXx");
-                    preparedStmt.setString (2, "passwort123");
-                    preparedStmt.setString   (3, "h1@vmail.de");
-                    preparedStmt.setString(4, "Hans");
-                    preparedStmt.setString    (5, "Jorgenssen");
-                    preparedStmt.setLong(6, 1361);
-                    preparedStmt.setBoolean(7, true);
-                    preparedStmt.setDate(8, startDate);
-                    preparedStmt.setDate(9, startDate);
-
-                    // execute the preparedstatement
-                    preparedStmt.execute();
-
-                    /*
-                    String query = "select * from users;";
-
-
-                    rs = st.executeQuery(query);
-
-                    while(rs.next()) {
-                        String name = rs.getString("Username");
-                        String fname = rs.getString("FirstName");
-                        String nname = rs.getString("LastName");
-                        System.out.println("User: " + name + "   Vorname: " + fname + "   Nachname: " + nname);
-                    }*/
-
-                    System.out.println("test");
-
-                    /*
-                    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-                    StrictMode.setThreadPolicy(policy);
-
-                    CookieHandler.setDefault( new CookieManager( null, CookiePolicy.ACCEPT_ALL ));
-
-                    URL url = new URL("http://141.59.26.107/phpScripts/InsertValue");
-
-                    URLConnection conn = url.openConnection();
-
-                    conn.setDoOutput(true);
-                    conn.setDoInput(true);
-
-                    conn.connect();
-
-                    BufferedOutputStream out = new BufferedOutputStream(conn.getOutputStream());
-
-
-                    for(int i = 0;i<100;i++) {
-                        out.write(i);
-                    }
-                    System.out.println("finished");
-                    /*BufferedReader br = new BufferedReader(
-                            new InputStreamReader(
-                                    conn.getInputStream()));
-                    String line;
-
-                    String result = "";
-
-                    while ((line = br.readLine()) != null)
-                        result += line + "\n";
-                    System.out.println(result);
-
-                    br.close();*/
-
-                }catch (Exception e){
-                    System.out.print("Fehler");
-                    e.printStackTrace();
-                }
+                SqlManager.getInstance().registerUser(firstname, lastname, email,username,password);
             }
-        });
 
+        });
     }
+
 }
--- app/src/main/java/ulm/hochschule/project_hoops/SqlManager.java
+++ app/src/main/java/ulm/hochschule/project_hoops/SqlManager.java
@@ -1,22 +1,93 @@
 package ulm.hochschule.project_hoops;
 
+import android.os.StrictMode;
+
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.net.CookiePolicy;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLInvalidAuthorizationSpecException;
+import java.sql.Statement;
+import java.util.Calendar;
 
 /**
- * Created by Johann on 14.05.2016.
+ * Created by Johann /PatrickW on 14.05.2016.
  */
 public class SqlManager {
 
+    private StrictMode.ThreadPolicy policy;
+    private Connection con;
+    private Statement st;
+    private ResultSet rs;
+    private java.sql.Date startDate;
+    private String query;
+
+
     //create an object of SingleObject
-    private static SqlManager instance = new SqlManager();
+    private static SqlManager instance ;
 
     //make the constructor private so that this class cannot be
     //instantiated
-    private SqlManager(){}
+    private SqlManager(){
+        try {
+            policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+            CookieHandler.setDefault(new CookieManager(null, CookiePolicy.ACCEPT_ALL));
+            Connection con;
+
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://141.59.26.107:3306/hoops", "SuperUser", "root");
+            st = con.createStatement();
+
+
+
+
+
+        }catch (Exception e){
+            System.out.print("Fehler");
+            e.printStackTrace();
+        }
+
+    }
 
     //Get the only object available
-    public static SqlManager getInstance(){
+    public static SqlManager getInstance()
+    {
+        if(instance == null)
+        {
+            instance = new SqlManager();
+        }
         return instance;
+    }
+
+
+    public void registerUser(String firstName,String lastName,String eMail, String userName, String password)
+    {
+        try {
+            Calendar calendar = Calendar.getInstance();
+            startDate = new java.sql.Date(calendar.getTime().getTime());
+            // the mysql insert statement
+            query = " insert into users (Username, Password, EmailAdress, FirstName, LastName, Coins, ChatBan, CreateDate, LastLogin)"
+                    + " values (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+// create the mysql insert preparedstatement
+            PreparedStatement preparedStmt = con.prepareStatement(query);
+
+            preparedStmt.setString(1, userName);
+            preparedStmt.setString(2, password);
+            preparedStmt.setString(3, eMail);
+            preparedStmt.setString(4, firstName);
+            preparedStmt.setString(5, lastName);
+            preparedStmt.setLong(6, 1361);
+            preparedStmt.setBoolean(7, true);
+            preparedStmt.setDate(8, startDate);
+            preparedStmt.setDate(9, startDate);
+        }catch (Exception e){
+            System.out.print("Fehler");
+            e.printStackTrace();
+        }
     }
 
     public void add(User user){
